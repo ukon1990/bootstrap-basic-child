f7fcf1276e2e3c9eae1ca497359b85fd182faade
Jonas <mail@jonaskf.net>
2015-05-19 14:39:43 +0200

    initialupload
    initialupload

---
+++ functions.php
@@ -0,0 +1,317 @@
+<?php
+/**
+ * Bootstrap Basic theme
+ * 
+ * @package bootstrap-basic
+ */
+
+
+/**
+ * Required WordPress variable.
+ */
+if (!isset($content_width)) {
+	$content_width = 1170;
+}
+
+
+if (!function_exists('bootstrapBasicSetup')) {
+	/**
+	 * Setup theme and register support wp features.
+	 */
+	function bootstrapBasicSetup() 
+	{
+		/**
+		 * Make theme available for translation
+		 * Translations can be filed in the /languages/ directory
+		 * 
+		 * copy from underscores theme
+		 */
+		load_theme_textdomain('bootstrap-basic', get_template_directory() . '/languages');
+
+		// add theme support post and comment automatic feed links
+		add_theme_support('automatic-feed-links');
+
+		// enable support for post thumbnail or feature image on posts and pages
+		add_theme_support('post-thumbnails');
+
+		// add support menu
+		register_nav_menus(array(
+			'primary' => __('Primary Menu', 'bootstrap-basic'),
+		));
+
+		// add post formats support
+		add_theme_support('post-formats', array('aside', 'image', 'video', 'quote', 'link'));
+
+		// add support custom background
+		add_theme_support(
+			'custom-background', 
+			apply_filters(
+				'bootstrap_basic_custom_background_args', 
+				array(
+					'default-color' => 'ffffff', 
+					'default-image' => ''
+				)
+			)
+		);
+	}// bootstrapBasicSetup
+}
+add_action('after_setup_theme', 'bootstrapBasicSetup');
+
+
+if (!function_exists('bootstrapBasicWidgetsInit')) {
+	/**
+	 * Register widget areas
+	 */
+	function bootstrapBasicWidgetsInit() 
+	{
+		register_sidebar(array(
+			'name'          => __('Header right', 'bootstrap-basic'),
+			'id'            => 'header-right',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h1 class="widget-title">',
+			'after_title'   => '</h1>',
+		));
+
+		register_sidebar(array(
+			'name'          => __('Navigation bar right', 'bootstrap-basic'),
+			'id'            => 'navbar-right',
+			'before_widget' => '',
+			'after_widget'  => '',
+			'before_title'  => '',
+			'after_title'   => '',
+		));
+
+		register_sidebar(array(
+			'name'          => __('Sidebar left', 'bootstrap-basic'),
+			'id'            => 'sidebar-left',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<h1 class="widget-title">',
+			'after_title'   => '</h1>',
+		));
+
+		register_sidebar(array(
+			'name'          => __('Sidebar right', 'bootstrap-basic'),
+			'id'            => 'sidebar-right',
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<h1 class="widget-title">',
+			'after_title'   => '</h1>',
+		));
+
+		register_sidebar(array(
+			'name'          => __('Footer left', 'bootstrap-basic'),
+			'id'            => 'footer-left',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h1 class="widget-title">',
+			'after_title'   => '</h1>',
+		));
+
+		register_sidebar(array(
+			'name'          => __('Footer right', 'bootstrap-basic'),
+			'id'            => 'footer-right',
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</div>',
+			'before_title'  => '<h1 class="widget-title">',
+			'after_title'   => '</h1>',
+		));
+	}// bootstrapBasicWidgetsInit
+}
+add_action('widgets_init', 'bootstrapBasicWidgetsInit');
+
+
+if (!function_exists('bootstrapBasicEnqueueScripts')) {
+	/**
+	 * Enqueue scripts & styles
+	 */
+	function bootstrapBasicEnqueueScripts() 
+	{
+		wp_enqueue_style('bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css');
+		wp_enqueue_style('bootstrap-theme-style', get_template_directory_uri() . '/css/bootstrap-theme.min.css');
+		wp_enqueue_style('fontawesome-style', get_template_directory_uri() . '/css/font-awesome.min.css');
+		wp_enqueue_style('main-style', get_template_directory_uri() . '/css/main.css');
+
+		wp_enqueue_script('modernizr-script', get_template_directory_uri() . '/js/vendor/modernizr.min.js');
+		wp_enqueue_script('respond-script', get_template_directory_uri() . '/js/vendor/respond.min.js');
+		wp_enqueue_script('html5-shiv-script', get_template_directory_uri() . '/js/vendor/html5shiv.js');
+		wp_enqueue_script('jquery');
+		wp_enqueue_script('bootstrap-script', get_template_directory_uri() . '/js/vendor/bootstrap.min.js');
+		wp_enqueue_script('main-script', get_template_directory_uri() . '/js/main.js');
+		wp_enqueue_style('bootstrap-basic-style', get_stylesheet_uri());
+	}// bootstrapBasicEnqueueScripts
+}
+add_action('wp_enqueue_scripts', 'bootstrapBasicEnqueueScripts');
+
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+
+/**
+ * Custom functions that act independently of the theme templates.
+ */
+require get_template_directory() . '/inc/extras.php';
+
+
+/**
+ * Custom dropdown menu and navbar in walker class
+ */
+require get_template_directory() . '/inc/BootstrapBasicMyWalkerNavMenu.php';
+
+
+/**
+ * Template functions
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+
+/**
+ * --------------------------------------------------------------
+ * Theme widget & widget hooks
+ * --------------------------------------------------------------
+ */
+require get_template_directory() . '/inc/widgets/BootstrapBasicSearchWidget.php';
+require get_template_directory() . '/inc/template-widgets-hook.php';
+
+function breadcrumbs(){
+    /* === OPTIONS === */
+    $text['home']     = 'Home'; // text for the 'Home' link
+    $text['category'] = 'Archive by Category "%s"'; // text for a category page
+    $text['tax'] 	  = 'Archive for "%s"'; // text for a taxonomy page
+    $text['search']   = 'Search Results for "%s" Query'; // text for a search results page
+    $text['tag']      = 'Posts Tagged "%s"'; // text for a tag page
+    $text['author']   = 'Articles Posted by %s'; // text for an author page
+    $text['404']      = 'Error 404'; // text for the 404 page
+
+    $showCurrent = 1; // 1 - show current post/page title in breadcrumbs, 0 - don't show
+    $showOnHome  = 0; // 1 - show breadcrumbs on the homepage, 0 - don't show
+    $delimiter   = ' &raquo; '; // delimiter between crumbs
+    $before      = '<span class="current">'; // tag before the current crumb
+    $after       = '</span>'; // tag after the current crumb
+    /* === END OF OPTIONS === */
+
+    global $post;
+    $homeLink = get_bloginfo('url') . '/';
+    $linkBefore = '<span typeof="v:Breadcrumb">';
+    $linkAfter = '</span>';
+    $linkAttr = ' rel="v:url" property="v:title"';
+    $link = $linkBefore . '<a' . $linkAttr . ' href="%1$s">%2$s</a>' . $linkAfter;
+
+    if (is_home() || is_front_page()) {
+
+        if ($showOnHome == 1) echo '<div id="crumbs"><a href="' . $homeLink . '">' . $text['home'] . '</a></div>';
+
+    } else {
+
+        echo '<div id="crumbs" xmlns:v="http://rdf.data-vocabulary.org/#">' . sprintf($link, $homeLink, $text['home']) . $delimiter;
+
+
+        if ( is_category() ) {
+            $thisCat = get_category(get_query_var('cat'), false);
+            if ($thisCat->parent != 0) {
+                $cats = get_category_parents($thisCat->parent, TRUE, $delimiter);
+                $cats = str_replace('<a', $linkBefore . '<a' . $linkAttr, $cats);
+                $cats = str_replace('</a>', '</a>' . $linkAfter, $cats);
+                echo $cats;
+            }
+            echo $before . sprintf($text['category'], single_cat_title('', false)) . $after;
+
+        } elseif( is_tax() ){
+            $thisCat = get_category(get_query_var('cat'), false);
+            if ($thisCat->parent != 0) {
+                $cats = get_category_parents($thisCat->parent, TRUE, $delimiter);
+                $cats = str_replace('<a', $linkBefore . '<a' . $linkAttr, $cats);
+                $cats = str_replace('</a>', '</a>' . $linkAfter, $cats);
+                echo $cats;
+            }
+            echo $before . sprintf($text['tax'], single_cat_title('', false)) . $after;
+
+        }elseif ( is_search() ) {
+            echo $before . sprintf($text['search'], get_search_query()) . $after;
+
+        } elseif ( is_day() ) {
+            echo sprintf($link, get_year_link(get_the_time('Y')), get_the_time('Y')) . $delimiter;
+            echo sprintf($link, get_month_link(get_the_time('Y'),get_the_time('m')), get_the_time('F')) . $delimiter;
+            echo $before . get_the_time('d') . $after;
+
+        } elseif ( is_month() ) {
+            echo sprintf($link, get_year_link(get_the_time('Y')), get_the_time('Y')) . $delimiter;
+            echo $before . get_the_time('F') . $after;
+
+        } elseif ( is_year() ) {
+            echo $before . get_the_time('Y') . $after;
+
+        } elseif ( is_single() && !is_attachment() ) {
+            if ( get_post_type() != 'post' ) {
+                $post_type = get_post_type_object(get_post_type());
+                $slug = $post_type->rewrite;
+                printf($link, $homeLink . '/' . $slug['slug'] . '/', $post_type->labels->singular_name);
+                if ($showCurrent == 1) echo $delimiter . $before . get_the_title() . $after;
+            } else {
+                $cat = get_the_category(); $cat = $cat[0];
+                $cats = get_category_parents($cat, TRUE, $delimiter);
+                if ($showCurrent == 0) $cats = preg_replace("#^(.+)$delimiter$#", "$1", $cats);
+                $cats = str_replace('<a', $linkBefore . '<a' . $linkAttr, $cats);
+                $cats = str_replace('</a>', '</a>' . $linkAfter, $cats);
+                echo $cats;
+                if ($showCurrent == 1) echo $before . get_the_title() . $after;
+            }
+
+        } elseif ( !is_single() && !is_page() && get_post_type() != 'post' && !is_404() ) {
+            $post_type = get_post_type_object(get_post_type());
+            echo $before . $post_type->labels->singular_name . $after;
+
+        } elseif ( is_attachment() ) {
+            $parent = get_post($post->post_parent);
+            $cat = get_the_category($parent->ID); $cat = $cat[0];
+            $cats = get_category_parents($cat, TRUE, $delimiter);
+            $cats = str_replace('<a', $linkBefore . '<a' . $linkAttr, $cats);
+            $cats = str_replace('</a>', '</a>' . $linkAfter, $cats);
+            echo $cats;
+            printf($link, get_permalink($parent), $parent->post_title);
+            if ($showCurrent == 1) echo $delimiter . $before . get_the_title() . $after;
+
+        } elseif ( is_page() && !$post->post_parent ) {
+            if ($showCurrent == 1) echo $before . get_the_title() . $after;
+
+        } elseif ( is_page() && $post->post_parent ) {
+            $parent_id  = $post->post_parent;
+            $breadcrumbs = array();
+            while ($parent_id) {
+                $page = get_page($parent_id);
+                $breadcrumbs[] = sprintf($link, get_permalink($page->ID), get_the_title($page->ID));
+                $parent_id  = $page->post_parent;
+            }
+            $breadcrumbs = array_reverse($breadcrumbs);
+            for ($i = 0; $i < count($breadcrumbs); $i++) {
+                echo $breadcrumbs[$i];
+                if ($i != count($breadcrumbs)-1) echo $delimiter;
+            }
+            if ($showCurrent == 1) echo $delimiter . $before . get_the_title() . $after;
+
+        } elseif ( is_tag() ) {
+            echo $before . sprintf($text['tag'], single_tag_title('', false)) . $after;
+
+        } elseif ( is_author() ) {
+            global $author;
+            $userdata = get_userdata($author);
+            echo $before . sprintf($text['author'], $userdata->display_name) . $after;
+
+        } elseif ( is_404() ) {
+            echo $before . $text['404'] . $after;
+        }
+
+        if ( get_query_var('paged') ) {
+            if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ' (';
+            echo __('Page') . ' ' . get_query_var('paged');
+            if ( is_category() || is_day() || is_month() || is_year() || is_search() || is_tag() || is_author() ) echo ')';
+        }
+
+        echo '</div>';
+
+    }
+} // end breadcrumbs
