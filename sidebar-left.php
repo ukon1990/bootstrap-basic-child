0107fa6dd4799fc0df31abda2429333d1297c63d
Jonas <mail@jonaskf.net>
2015-05-20 20:53:02 +0200

    Kategori fiks
    Hvis kategorien ikke eksisterer, ikke vis en haug med tull isteden, vis
    blank boks.

--- sidebar-left.php
+++ sidebar-left.php
@@ -13,12 +13,14 @@
                 echo '<ul>'.$this_category.'</ul>';
             }
         }else if (is_page()){
-            $cat = get_cat_ID(get_the_title());
-            $this_category = get_category($cat);
-            $this_category = wp_list_categories('hide_empty=0&hierarchical=true&orderby=id&show_count=0&title_li=&use_desc_for_title=1&child_of='.$this_category->cat_ID."&echo=0");
-            if($this_category !='<li>No categories</li>')
-            {
-                echo '<ul>'.$this_category.'</ul>';
+            if(term_exists(get_cat_ID(get_the_title())) == true){
+                $cat = get_cat_ID(get_the_title());
+                $this_category = get_category($cat);
+                $this_category = wp_list_categories('hide_empty=0&hierarchical=true&orderby=id&show_count=0&title_li=&use_desc_for_title=1&child_of='.$this_category->cat_ID."&echo=0");
+                if($this_category !='<li>No categories</li>')
+                {
+                    echo '<ul>'.$this_category.'</ul>';
+                }
             }
         }else{
             echo '<p>Fix</p>';
