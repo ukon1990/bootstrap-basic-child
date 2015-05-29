1f38e6acc2cd6ac0a752ad0c5ccbf723698278de
ukon1990 <mail@jonaskf.net>
2015-05-29 14:27:51 +0200

    footer-minor fix - ikke ferdig!

--- footer.php
+++ footer.php
@@ -11,25 +11,33 @@
 
 <footer id="site-footer" role="contentinfo">
     <div id="footer-row" class="row site-footer">
-        <div class="col-md-6 footer-left">
-            <div class="footer-address">
-                <address>
-                    <span><strong>Daglig leder</strong>: Jan Ivar Melby</span></br>
-                    <span><strong>E-post</strong>: <a href="mailto:info@hjelleseter.no"">info@hjelleseter.no</a></span></br>
-                    <span><strong>Tlf</strong>: 61 24 16 95</span></br>
-                    <span><strong>Mobil</strong>: 41 54 24 80</span></br>
-                    <span><strong>Adresse</strong>: Dovre, 2660 Dombås</span></br>
-                </address
-            </div>
-            <?php
+        <div class="col-md-6 footer-left" style="width:40%;">
+            <!--<div class="footer-address"></div>-->
+            <address>
+                <span><strong>Daglig leder</strong>: Jan Ivar Melby</span></br>
+                <span><strong>E-post</strong>: <a href="mailto:info@hjelleseter.no"">info@hjelleseter.no</a></span></br>
+                <span><strong>Tlf</strong>: 61 24 16 95</span></br>
+                <span><strong>Mobil</strong>: 41 54 24 80</span></br>
+                <span><strong>Adresse</strong>: Dovre, 2660 Dombås</span></br>
+            </address>
+        </div><!--
+        <div style="float:left; margin-top:4em;">
+            <?php/*
             if (!dynamic_sidebar('footer-left')) {
                 printf(__('<p>Powered by %s', 'bootstrap-basic'), 'WordPress');
                 echo ' | ';
                 printf(__('Theme: %s', 'bootstrap-basic'), 'Hjelle seter</p>');
-            }
+            }*/
             ?>
-        </div>
-        <div class="col-md-6 footer-right text-right">
+        </div>-->
+        <div class="col-md-6 footer-right text-right" style="width:40%; float:right;">
+            <address>
+                <span><strong>Daglig leder</strong>: Jan Ivar Melby</span></br>
+                <span><strong>E-post</strong>: <a href="mailto:info@hjelleseter.no"">info@hjelleseter.no</a></span></br>
+                <span><strong>Tlf</strong>: 61 24 16 95</span></br>
+                <span><strong>Mobil</strong>: 41 54 24 80</span></br>
+                <span><strong>Adresse</strong>: Dovre, 2660 Dombås</span></br>
+            </address
             <?php dynamic_sidebar('footer-right'); ?>
         </div>
     </div>
