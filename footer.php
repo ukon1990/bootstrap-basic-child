b900b11b4e1be10de19acd19c0437eeffa1559f0
Jonas <mail@jonaskf.net>
2015-05-29 19:46:12 +0200

    footer fix

--- footer.php
+++ footer.php
@@ -11,31 +11,16 @@
 
 <footer id="site-footer" role="contentinfo">
     <div id="footer-row" class="row site-footer">
-        <div class="col-md-6 footer-left" style="width:40%;">
-            <!--<div class="footer-address"></div>-->
-            <address>
+        <div id="foot-left" class="col-md-6 footer-left"">
+            <address id="address-left"">
                 <span><strong>Daglig leder</strong>: Jan Ivar Melby</span></br>
-                <span><strong>E-post</strong>: <a href="mailto:info@hjelleseter.no"">info@hjelleseter.no</a></span></br>
-                <span><strong>Tlf</strong>: 61 24 16 95</span></br>
-                <span><strong>Mobil</strong>: 41 54 24 80</span></br>
-                <span><strong>Adresse</strong>: Dovre, 2660 Dombås</span></br>
+                <span><strong>Mobil</strong>: 41 54 24 80</span>
             </address>
-        </div><!--
-        <div style="float:left; margin-top:4em;">
-            <?php/*
-            if (!dynamic_sidebar('footer-left')) {
-                printf(__('<p>Powered by %s', 'bootstrap-basic'), 'WordPress');
-                echo ' | ';
-                printf(__('Theme: %s', 'bootstrap-basic'), 'Hjelle seter</p>');
-            }*/
-            ?>
-        </div>-->
-        <div class="col-md-6 footer-right text-right" style="width:40%; float:right;">
-            <address>
-                <span><strong>Daglig leder</strong>: Jan Ivar Melby</span></br>
+        </div>
+        <div id="foot-right" class="col-md-6 footer-right text-right">
+            <address id="address-right">
                 <span><strong>E-post</strong>: <a href="mailto:info@hjelleseter.no"">info@hjelleseter.no</a></span></br>
                 <span><strong>Tlf</strong>: 61 24 16 95</span></br>
-                <span><strong>Mobil</strong>: 41 54 24 80</span></br>
                 <span><strong>Adresse</strong>: Dovre, 2660 Dombås</span></br>
             </address
             <?php dynamic_sidebar('footer-right'); ?>
