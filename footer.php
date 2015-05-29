7c58597fd9d7d0c25c064b32897163a50fa9c977
ukon1990 <mail@jonaskf.net>
2015-05-29 07:01:25 +0200

    Fix for something?

--- footer.php
+++ footer.php
@@ -10,22 +10,22 @@
 
 
 <footer id="site-footer" role="contentinfo">
-    <div id="footer-address">
-        <address>
-            <span><strong>Daglig leder</strong>: Jan Ivar Melby</span></br>
-            <span><strong>E-post</strong>: <a href="mailto:info@hjelleseter.no"">info@hjelleseter.no</a></span></br>
-            <span><strong>Tlf</strong>: 61 24 16 95</span></br>
-            <span><strong>Mobil</strong>: 41 54 24 80</span></br>
-            <span><strong>Adresse</strong>: Dovre, 2660 Dombås</span></br>
-        </address
-    </div>
     <div id="footer-row" class="row site-footer">
         <div class="col-md-6 footer-left">
+            <div class="footer-address">
+                <address>
+                    <span><strong>Daglig leder</strong>: Jan Ivar Melby</span></br>
+                    <span><strong>E-post</strong>: <a href="mailto:info@hjelleseter.no"">info@hjelleseter.no</a></span></br>
+                    <span><strong>Tlf</strong>: 61 24 16 95</span></br>
+                    <span><strong>Mobil</strong>: 41 54 24 80</span></br>
+                    <span><strong>Adresse</strong>: Dovre, 2660 Dombås</span></br>
+                </address
+            </div>
             <?php
             if (!dynamic_sidebar('footer-left')) {
-                printf(__('Powered by %s', 'bootstrap-basic'), 'WordPress');
+                printf(__('<p>Powered by %s', 'bootstrap-basic'), 'WordPress');
                 echo ' | ';
-                printf(__('Theme: %s', 'bootstrap-basic'), '<a href="http://jonaskf.net">Hjelle seter</a>');
+                printf(__('Theme: %s', 'bootstrap-basic'), 'Hjelle seter</p>');
             }
             ?>
         </div>
